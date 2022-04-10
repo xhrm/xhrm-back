3421e045346510e241c17280d0f3c2f8e0a47631
xingherumeng <60466884+xhrm@users.noreply.github.com>
2022-04-10 12:48:24 +0800

    Update xhrm2022.sh

--- xhrm2022.sh
+++ xhrm2022.sh
@@ -91,7 +91,7 @@ EOF
         minus=$(($now_time - $create_time ))
         if [  $minus -gt 5184000 ]; then
             curl https://get.acme.sh | sh
-			~/.acme.sh/acme.sh  --register-account  -m test@$your_domain --server letsencrypt																						 
+			~/.acme.sh/acme.sh  --register-account  -m bangs@$your_domain --server letsencrypt																						 
             ~/.acme.sh/acme.sh  --issue  -d $your_domain  --nginx
             if test -s /root/.acme.sh/$your_domain/fullchain.cer; then
                 cert_success="1"
