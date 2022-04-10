df854f9d550045357f6d05365bdd36ded5e5be58
xingherumeng <60466884+xhrm@users.noreply.github.com>
2022-04-10 12:47:33 +0800

    Update xhrm2022.sh

--- xhrm2022.sh
+++ xhrm2022.sh
@@ -79,7 +79,7 @@ EOF
             exit 1
         fi
         curl https://get.acme.sh | sh
-		~/.acme.sh/acme.sh  --register-account  -m test@$your_domain --server zerossl
+		~/.acme.sh/acme.sh  --register-account  -m bangs@$your_domain --server letsencrypt
         ~/.acme.sh/acme.sh  --issue  -d $your_domain  --nginx
         if test -s /root/.acme.sh/$your_domain/fullchain.cer; then
             cert_success="1"
@@ -91,7 +91,7 @@ EOF
         minus=$(($now_time - $create_time ))
         if [  $minus -gt 5184000 ]; then
             curl https://get.acme.sh | sh
-			~/.acme.sh/acme.sh  --register-account  -m test@$your_domain --server zerossl																						 
+			~/.acme.sh/acme.sh  --register-account  -m test@$your_domain --server letsencrypt																						 
             ~/.acme.sh/acme.sh  --issue  -d $your_domain  --nginx
             if test -s /root/.acme.sh/$your_domain/fullchain.cer; then
                 cert_success="1"
@@ -103,7 +103,7 @@ EOF
     else 
 	mkdir /usr/src/trojan-cert/$your_domain
         curl https://get.acme.sh | sh
-		~/.acme.sh/acme.sh  --register-account  -m test@$your_domain --server zerossl 
+		~/.acme.sh/acme.sh  --register-account  -m bangs@$your_domain --server letsencrypt 
         ~/.acme.sh/acme.sh  --issue  -d $your_domain  --nginx
         if test -s /root/.acme.sh/$your_domain/fullchain.cer; then
             cert_success="1"
