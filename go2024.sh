85b98a401358a483d10f92f56463463626632137
xingherumeng <60466884+xhrm@users.noreply.github.com>
2024-07-31 12:11:50 +0800

    Update go2024.sh

--- go2024.sh
+++ go2024.sh
@@ -563,7 +563,7 @@ configTrojan() {
     "plain_http_response": "",
     "fallback_addr": "",
     "fallback_port": 0,
-    "fingerprint": "chrome"
+    "fingerprint": "ios"
   },
   "tcp": {
     "no_delay": true,
